6380bf7bf51644c1a675a26b0446155c553ffde3
Brotella <shevchuk-savva@yandex.ru>
2017-12-11 23:25:29 +0300

    Updated Tests

--- Rubik.cpp
+++ Rubik.cpp
@@ -5,31 +5,45 @@
 #include "OLL.hpp"
 #include "PLL.hpp"
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <cassert>
+#include <ctime>
 
 std::string format(std::string);
 
 int main(int argc, char **argv) {
-
+    if (argc < 2) {
+        return 0;
+    }
+    
+    std::ifstream fileIn(argv[1]);
+    assert(fileIn);
+    
     Cube::Cube myCube(false);
     int scrambleNum = 0;
     std::string argString;
-    while (std::getline(std::cin, argString)) {
-    ++scrambleNum;
-    std::string scramble = format(argString);
-    std::cout << "Scramble #" << scrambleNum << ": ";
-    myCube.moves(scramble);
-    Cross::solveCross(myCube);
-    std::cout << "Cross solved" << std::endl;
-    Corners::solveCorners(myCube);
-    std::cout << "Corners solved" << std::endl;
-    Edges::solveEdges(myCube);
-    std::cout << "Edges solved" << std::endl;;
-    OLL::solveOLL(myCube);
-    std::cout << "OLL solved" << std::endl;
-    PLL::solvePLL(myCube);
-    std::cout << "PLL solved" << std::endl;
-  }
+    
+    srand(time(0));
+    
+    while (std::getline(fileIn, argString)) {
+        ++scrambleNum;
+        std::string scramble = format(argString);
+        std::cout << "Scramble #" << scrambleNum << ": ";
+        myCube.moves(scramble);
+        Cross::solveCross(myCube);
+        std::cout << "Cross solved" << std::endl;
+        Corners::solveCorners(myCube);
+        std::cout << "Corners solved" << std::endl;
+        Edges::solveEdges(myCube);
+        std::cout << "Edges solved" << std::endl;;
+        OLL::solveOLL(myCube);
+        std::cout << "OLL solved" << std::endl;
+        PLL::solvePLL(myCube);
+        std::cout << "PLL solved" << std::endl;
+    }
+    
+    std::cout << "Count of tests: " << scrambleNum <<  " Time = " << clock()/1000.0 << std::endl;
+    
   return 0;
 }
 
